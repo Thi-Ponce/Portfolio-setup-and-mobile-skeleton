a6869f98ee193dbef2f82630d0a42f9ebc108137
Thi-Ponce <thi.pponce@gmail.com>
2022-01-24 18:17:02 -0300

    add linters and finish js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".desktop-only");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.desktop-only');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
