2cc49443590d5f489ee9d9b4c31f41bf1f736d67
Thi-Ponce <thi.pponce@gmail.com>
2022-01-26 16:34:15 -0300

    Finish popup styling

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+ //activate popup1
+const open1 = document.getElementById('open1');
+const modalContainer1 = document.getElementById('modal1');
+const overlay = document.getElementById('overlay');
+const close1 = document.getElementById('close1');
+
+open1.addEventListener('click', () => {
+  modalContainer1.classList.add('active');
+  overlay.classList.add('active');
+});
+
+close1.addEventListener('click', () => {
+  modalContainer1.classList.remove('active');
+  overlay.classList.remove('active');
+});
