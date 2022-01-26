02657e106a336d0e565a820f6a4df78656f7e494
yersel <yersel500@gmail.com>
2022-01-26 16:04:41 -0500

    Fix card and modal 2

--- script.js
+++ script.js
@@ -26,6 +26,21 @@ close1.addEventListener('click', () => {
   overlay.classList.remove('active');
 });
 
+ //activate popup2
+ const open2 = document.getElementById('open2');
+ const modalContainer2 = document.getElementById('modal2');
+ const close2 = document.getElementById('close2');
+ 
+ open2.addEventListener('click', () => {
+   modalContainer2.classList.add('active');
+   overlay.classList.add('active');
+ });
+ 
+ close2.addEventListener('click', () => {
+   modalContainer2.classList.remove('active');
+   overlay.classList.remove('active');
+ });
+
 // create the js object
 function Project(name, description, image, languages, live, source) {
   this.projectName = name;
@@ -99,4 +114,32 @@ document.getElementById('lg1-2').innerHTML = project2.projectLanguages.lg1;
 document.getElementById('lg2-2').innerHTML = project2.projectLanguages.lg2;
 
 document.getElementById('lg3-2').innerHTML = project2.projectLanguages.lg3;
+
+// Modal 2 obj
+const project2m = new Project('Multi-Post Stories', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", './Exported_elements/Images/Snapshoot_Portfolio_2.png', {
+  lg1: 'html', lg2: 'css', lg3: 'javaScript', lg4: 'github', lg5: 'ruby', lg6: ' bootstraps',
+},
+'https://thi-ponce.github.io/Portfolio-setup-and-mobile-skeleton/', 'https://github.com/Thi-Ponce/Portfolio-setup-and-mobile-skeleton');
+
+document.getElementById('title2-m').innerHTML = project2m.projectName;
+
+document.getElementById('descrip2-m').innerHTML = project2m.projectDescription;
+
+document.getElementById('img2-m').src = project2m.projectImage;
+
+document.getElementById('lg1-m-2').innerHTML = project2m.projectLanguages.lg1;
+
+document.getElementById('lg2-m-2').innerHTML = project2m.projectLanguages.lg2;
+
+document.getElementById('lg3-m-2').innerHTML = project2m.projectLanguages.lg3;
+
+document.getElementById('lg4-m-2').innerHTML = project2m.projectLanguages.lg4;
+
+document.getElementById('lg5-m-2').innerHTML = project2m.projectLanguages.lg5;
+
+document.getElementById('lg6-m-2').innerHTML = project2m.projectLanguages.lg6;
+
+document.getElementById('live-m-2').href = project2m.projectLive;
+
+document.getElementById('source-m-2').href = project2m.projectSource;
 
