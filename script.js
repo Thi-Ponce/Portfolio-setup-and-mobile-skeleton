c37434d7359a0be6fbc285b84706c4de811cb233
yersel <yersel500@gmail.com>
2022-01-26 15:07:22 -0500

    Add js object for card 1

--- script.js
+++ script.js
@@ -25,3 +25,30 @@ close1.addEventListener('click', () => {
   modalContainer1.classList.remove('active');
   overlay.classList.remove('active');
 });
+
+// create the js object
+function Project(name, description, image, languages, live, source) {
+  this.projectName = name;
+  this.projectDescription = description;
+  this.projectImage = image;
+  this.projectLanguages = languages;
+  this.projectLive = live;
+  this.projectSource = source;
+}
+
+const project1 = new Project('Tonic', 'A daily selection of privately personalized reads; no accounts or sign-ups required.', './Exported_elements/Images/Snapshoot_Portfolio_1st_card.png', {
+  lg1: 'html', lg2: 'css', lg3: 'javaScript',
+},
+'https://yersel500.github.io/portfolio/', 'https://github.com/yersel500/portfolio');
+
+document.getElementById('title1').innerHTML = project1.projectName;
+
+document.getElementById('descrip1').innerHTML = project1.projectDescription;
+
+document.getElementById('img1').src = project1.projectImage;
+
+document.getElementById('lg1').innerHTML = project1.projectLanguages.lg1;
+
+document.getElementById('lg2').innerHTML = project1.projectLanguages.lg2;
+
+document.getElementById('lg3').innerHTML = project1.projectLanguages.lg3;
